1b492fa03858053033c6b7875b861f9c9a1b0758
YanLee1980 <9976592@qq.com>
2020-11-15 23:54:44 +0800

    Week9 Assignment

--- Week_09/longestPalindrome.py
+++ Week_09/longestPalindrome.py
@@ -2,6 +2,7 @@
 # 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
 # https://leetcode-cn.com/problems/longest-palindromic-substring/
 
+# DP:
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         n = len(s)
@@ -16,3 +17,25 @@ class Solution:
         
         return res
         
+# 枚举中心，两边扩散:
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        def _extendPalin(i, j):
+            nonlocal lo, max_len
+        
+            while i >= 0 and j < n and s[i] == s[j]:
+                i -= 1
+                j += 1
+            if j - i - 1 > max_len:
+                lo = i + 1
+                max_len = j - i - 1
+            
+        n = len(s)
+        if n < 2: return s
+        lo, max_len = 0, 0
+        
+        for i in range(n):
+            _extendPalin(i, i)
+            _extendPalin(i, i+1)
+            
+        return s[lo: lo+max_len]
